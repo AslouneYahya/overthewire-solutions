51374480d61be87f9bb219741696e7bb17a47ede
Yahya Asloune <146072190+AslouneYahya@users.noreply.github.com>
2026-01-02 15:18:48 +0100

    Fix markdown syntax for level page links

--- changingProblemExpressionInEachLevel.sh
+++ changingProblemExpressionInEachLevel.sh
@@ -2,11 +2,11 @@
 for i in {0..9}
 do
 	sed -i '1s/.*/# Problem Link/' level0$i/README.md
-	sed -i "1a [Level Page](https://overthewire.org/wargames/bandit/bandit$i.html" level0$i/README.md
+	sed -i "1a [Level Page](https://overthewire.org/wargames/bandit/bandit$i.html)" level0$i/README.md
 done
 
 for i in {10..34}
 do
 	sed -i '1s/.*/# Problem Link/' level$i/README.md
-	sed -i "1a [Level Page](https://overthewire.org/wargames/bandit/bandit$i.html" level$i/README.md
+	sed -i "1a [Level Page](https://overthewire.org/wargames/bandit/bandit$i.html)" level$i/README.md
 done
